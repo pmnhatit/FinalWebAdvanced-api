40f4a720143565fd8227fe0125291e9cdb522f5c
nbdkhang <nbdkhang@gmail.com>
2021-01-02 18:02:44 +0700

    [user] edit profile + change password

--- model/user/user.model.js
+++ model/user/user.model.js
@@ -11,22 +11,55 @@ module.exports.getUserByID= async (id) =>{
     return result;
 }
 
-module.exports.updateUser = async (id, name, phone) => {
-    const result = await userModel.updateOne({ '_id': id }, { $set: { 'name': name, 'phone': phone }}, (err, doc) => {
+module.exports.updateUserInfo = async (id, name, phone,password,matches,trophies,win_rate) => {
+    if(name!=="")
+    {   //console.log("name not null");
+        const result = await userModel.updateOne({ '_id': id }, { $set: { 'name': name, 'phone': phone }}, (err, doc) => {
+                if (err) {
+                    console.log("update user error");
+                } else {
+                    console.log("update user success");
+                    //console.log(doc);
+                }
+            });
+    }
+    if(password!=="")
+    {
+        console.log("pass not null");
+       const result = await userModel.updateOne({ '_id': id }, { $set: { 'password': password }}, (err, doc) => {
         if (err) {
-            console.log("update document error");
+            console.log("update pass error");
         } else {
-            console.log("update document success");
-            console.log(doc);
+            console.log("update pass success");
+            //console.log(doc);
         }
     });
-    console.log("hết service");
-}
+    }
+    if(matches!=="")
+    {
+        console.log("matches not null");
+        const result = await userModel.updateOne({ '_id': id }, { $set: { 'matches':matches,'trophies': trophies, 'win_rate': win_rate }}, (err, doc) => {
+                if (err) {
+                    console.log("update match error");
+                } else {
+                    console.log("update match success");
+                    //console.log(doc);
+                }
+            });
+    }
+     
+     console.log("hết service");
+     
+},
+
+
 
 module.exports.createUser = async (username, password, name, phone, email) =>{
-    console.log("pass"+password);
+   
     let hash = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
-    console.log(hash);
-    const newUser = new userModel({ username, password: hash, name, phone, email});
+    const trophies=1000;
+    const matches=0;
+    const win_rate=100;
+    const newUser = new userModel({ username, password: hash, name, phone, email,matches,trophies,win_rate});
     return newUser.save();
 }
--- routes/users.js
+++ routes/users.js
@@ -153,25 +153,7 @@ router.post("/signup", async (req, res) => {
     res.status(401).json({ message: "errors", error: error });
   }
 });
-router.post(
-  "/edit",
-  passport.authenticate("jwt", { session: false }),
-  async (req, res) => {
-    const entity = req.body;
-    console.log(entity);
-    //const result= await userModel.patch(entity);
-  }
-);
 
-router.get(
-  "/profile",
-  passport.authenticate("jwt", { session: false }),
-  async (req, res) => {
-    const entity = req.body;
-    console.log(entity);
-    const result= await userModel.getUserByID(entity.id);
-    console.log(result);
-    //const result= await userModel.patch(entity);
-  }
-);
+
+
 module.exports = router;
